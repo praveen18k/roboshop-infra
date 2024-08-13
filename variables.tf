2bff5940e89c35f059c128bed3823e3ebf1eb7de
Praveen <praveenkothapally18@gmail.com>
2024-08-14 01:19:59 +0530

    added public subnet to vpc module

--- variables.tf
+++ variables.tf
@@ -20,5 +20,16 @@ variable "internet_gateway_tags" {
   default = {
     Name = "roboshop"
   }
+}
 
+variable "public_subnet_cidr" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_names" {
+  default = ["roboshop-public-1a", "roboshop-public-1b"]
 }
--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,13 @@ module "this" {
   source      = "../terraform-module-vpc"
   cidr_block  = var.cidr_block
   common_tags = var.common_tags
-  vpc_tags = var.vpc_tags
-  
+  vpc_tags    = var.vpc_tags
+  igw_tags    = var.internet_gateway_tags
+
+  # public subnet creation
+  public_subnet_cidr  = var.public_subnet_cidr
+  azs                 = var.azs
+  public_subnet_names = var.public_subnet_names
+
 }
 
